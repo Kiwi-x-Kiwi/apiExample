cd5fc9c3e4007ba5d76be8ecc9ef3dc2f2d11711
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-09-13 16:14:34 -0400

    Added Marcos example

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const axios = require("axios");
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  axios.get("https://pokeapi.co/api/v2/pokemon/1")
+  .then(responseFromApi => {
+    console.log(responseFromApi.data);
+    // res.send(responseFromApi.data);
+    data = {
+      name: responseFromApi.data.name,
+      type: responseFromApi.data.types
+    };
+    res.render('api', data);
+  })
+  .catch(err => console.error(err));
+});
+
+module.exports = router;
